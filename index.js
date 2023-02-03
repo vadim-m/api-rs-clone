db8b601a5687feb3761126f5854719b812aa305a
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2023-02-03 20:15:02 +0300

    feat: add database config data

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const PORT = 3000;
+const DB_USER = "rs-client";
+const DB_PASS = "ohPiDM9NUxzSxawj";
+const DB_URL = `mongodb+srv://${DB_USER}:${DB_PASS}@rs-cluster.zay4fzg.mongodb.net/?retryWrites=true&w=majority`;
 
 const app = express();
 app.use(express.json());
