2ee10796acbc89e01e5e0913917f220db8ee8da1
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2023-02-17 17:32:04 +0300

    feat: change User schema

--- models/User.js
+++ models/User.js
@@ -1,14 +1,17 @@
 import { Schema, model } from "mongoose";
 
-const User = Schema({
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
-  fullName: { type: String, required: true },
-  registrationDate: { type: Date, default: Date.now },
-  carId: { type: String, default: "0" },
-  language: { type: String, default: "RU" },
-  currency: { type: String, default: "₽" },
-  units: { type: String, default: "Europe" },
-});
+const User = Schema(
+  {
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    fullName: { type: String, required: true },
+    hasCar: { type: Boolean, default: false },
+    carId: { type: String },
+    language: { type: String, default: "RU" },
+    currency: { type: String, default: "₽" },
+    units: { type: String, default: "Europe" },
+  },
+  { timestamps: true }
+);
 
 export default model("User", User);
